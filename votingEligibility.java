591978a9a4b4f5fecb767632b9d6836cb740986a
dimpal Rakesh <dimpeer17@gmail.com>
2024-05-08 19:30:35 +0530

    new change add by dimpee

--- votingEligibility.java
+++ votingEligibility.java
@@ -4,8 +4,20 @@ public class votingEligibility {
 
 	public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        
+     int age = 20;
+
+    if (age >= 18) 
+    {
+    	
+     System.out.println("You are eligible to vote.");
+     
+     } 
+    else
+    {
+       System.out.println("You are not eligible to vote.");
+    }
+     }
+ }
 
 
-	}
-
-}
